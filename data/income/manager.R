fc1d17ff1e4cb9809da5024dda2a187bc3660b78
jinbin <jinbin@weidian.com>
2017-04-01 11:16:40 +0800

    使用function简化代码

---
+++ data/income/manager.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+cal <- function(source){
+  file <- paste("./", source, ".csv", sep="")
+  source_data <- read.csv(file)
+
+  line <- paste("2017年", source, "次数: ", sep="")
+  print(line)
+  print(length(source_data$money))
+  print("总收入: ")
+  print(sum(source_data$money))
+  print("平均客单价: ")
+  print(mean(source_data$money))
+
+  return(sum(source_data$money))
+}
+
+sum1 = cal("顺风车")
+sum2 = cal("快车")
+sum3 = cal("刷单")
+
+print("2017年PartTime总收入: ")
+print(sum1 + sum2 + sum3)
