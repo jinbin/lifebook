48dba9f875b6082250b817c5484a7c44aa32664b
jinbin <jinbin@weidian.com>
2017-04-02 12:58:42 +0800

    优化代码

--- data/investment/investment.R
+++ data/investment/investment.R
@@ -6,11 +6,11 @@ stock <- read.csv("/Users/jinbin/lifebook/data/investment/股票.csv")
 
 sum <- 0
 h_last_price = hash()
+stock_list <- stock$stock[stock$date == "2016-12-30"]
 for(x in 1:length(stock$date)){
   if(stock$date[x] == "2016-12-30"){
     sum = sum + stock$share[x] * stock$price[x]
-    stock_list <- c(stock_list[1:x-1], gsub(" ", "", stock$stock[x]))
-    .set(h_last_price, keys = gsub(" ", "", stock$stock[x]), values = gsub(" ", "", stock$price[x]))
+    .set(h_last_price, keys = gsub(" ", "", stock_list[x]), values = stock$price[x])
   }
 }
 print(paste("2016年年底本金: ", sum, seq=""))
@@ -29,13 +29,7 @@ print(paste("2017年总投入资金: ", add + sum, seq=""))
 #获取每种stock的current_share
 h_current_share = hash()
 for(i in 1:length(stock_list)){
-  current_share <- 0
-  for(x in 1:length(stock$date)){
-    if(gsub(" ", "", stock$stock[x]) == stock_list[i]){
-      current_share = current_share + stock$share[x]
-    }
-  }
-  .set(h_current_share, keys = stock_list[i], values = current_share)
+  .set(h_current_share, keys = gsub(" ", "", stock_list[i]), values = sum(stock$share[stock$stock == stock_list[i]]))
 }
 
 #获取每种stock的current_price
@@ -46,22 +40,26 @@ for(x in 1:length(stock$date)){
   }
 }
 
+#.set(h_current_price, keys = stock$stock[stock$date == "N/A"], values = stock$price[stock$date == "N/A"])
+
 #获取每种stock的current_market_value
 current_sum <- 0
 print("2017年目前持股情况: ")
 for(i in 1:length(stock_list)){
-  if(h_current_share[[stock_list[i]]] != 0){
-    print(paste(stock_list[i], "持有股数: ", h_current_share[[stock_list[i]]]), seq="")
-    print(paste(stock_list[i], "每股现价: ", h_current_price[[stock_list[i]]]), seq="")
-    print(paste(stock_list[i], "每股涨幅(跌幅): ", strtrim((as.numeric(h_current_price[[stock_list[i]]]) - as.numeric(h_last_price[[stock_list[i]]]))/as.numeric(h_last_price[[stock_list[i]]]) * 100, 5), "%", seq=""))
-    current_sum = current_sum + h_current_share[[stock_list[i]]] * as.numeric(h_current_price[[stock_list[i]]])
+  if(h_current_share[[gsub(" ", "", as.character(stock_list[i]))]] != 0){
+    current_price <- as.numeric(h_current_price[[gsub(" ", "", as.character(stock_list[i]))]])
+    current_share <- as.numeric(h_current_share[[gsub(" ", "", as.character(stock_list[i]))]])
+    last_price <- as.numeric(h_last_price[[gsub(" ", "", as.character(stock_list[i]))]])
+    
+    print(paste(stock_list[i], "持有股数: ", current_share, seq=""))
+    print(paste(stock_list[i], "每股现价: ", current_price, seq=""))
+    print(paste(stock_list[i], "每股涨幅(跌幅): ", strtrim((current_price - last_price)/last_price * 100, 5), "%", seq=""))
+    current_sum = current_sum + current_price * current_share
   }
 }
 for(x in 1:length(stock$date)){
-  if(stock$date[x] != "2016-12-30"){
-    if(stock$share[x] < 0){
-      current_sum = current_sum - stock$share[x] * stock$price[x]
-    }
+  if(stock$share[x] < 0){
+    current_sum = current_sum - stock$share[x] * stock$price[x]
   }
 }
 
