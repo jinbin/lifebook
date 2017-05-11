169a364e9effffda855166081081adecaa8da158
jinbin <jinbin@weidian.com>
2017-05-11 13:44:13 +0800

    update data

--- data/investment/stock.R
+++ data/investment/stock.R
@@ -53,20 +53,26 @@ for(i in 1:length(stock_list)){
     
     print(paste(stock_list[i], "持有股数: ", current_share, seq=""))
     print(paste(stock_list[i], "每股现价: ", current_price, seq=""))
-    print(paste(stock_list[i], "每股涨幅(跌幅): ", strtrim((current_price - last_price)/last_price * 100, 5), "%", seq=""))
+    if(last_price != 0){    
+      print(paste(stock_list[i], "每股涨幅(跌幅): ", strtrim((current_price - last_price)/last_price * 100, 5), "%", seq=""))
+    }
     current_sum = current_sum + current_price * current_share
   }
 }
+
+print(paste("2017年当前持股市值: ", current_sum, seq=""))
+
 for(x in 1:length(stock$date)){
   if(stock$share[x] < 0){
     current_sum = current_sum - stock$share[x] * stock$price[x]
   }
 }
 
-print(paste("2017年总市值: ", current_sum, seq=""))
+print(paste("2017年总收入(持股市值+现金): ", current_sum, seq=""))
 
 profit <- current_sum - add - sum
 print(paste("2017年总盈利(亏损): ", profit, seq=""))
 
 growth_rate <- (current_sum - add - sum) / (add + sum) * 100
 print(paste("整体增长(下降)率: ", strtrim(growth_rate, 5), "%", seq=""))
+
