a79303d43ce0150a6e376511f430120e41e4d542
jinbin <jinbin@weidian.com>
2018-01-09 17:58:38 +0800

    新增2018数据

---
+++ data/2018/income/income.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env RScript
+
+cal <- function(source){
+  file <- paste("/Users/jinbin/lifebook/data/2018/income/", source, ".csv", sep="")
+  source_data <- read.csv(file)
+
+  line <- paste("2018年", source, "次数: ", sep="")
+  print(line)
+  print(length(source_data$money))
+  print("总收入: ")
+  print(sum(source_data$money))
+  print("平均客单价: ")
+  print(mean(source_data$money))
+
+  return(sum(source_data$money))
+}
+
+sum1 = cal("顺风车")
+sum2 = cal("其他")
+
+print("2018年PartTime总收入: ")
+print(sum1 + sum2)
